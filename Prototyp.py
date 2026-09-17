fb5d2fcb12882c89a8215933404c9e395dbf0030
podolskiy393740-sketch <podolskiy393740@gmail.com>
2026-09-17 10:14:57 +0300

    Описание изменений

--- Prototyp.py
+++ Prototyp.py
@@ -185,7 +185,7 @@ def rrf_hybrid_search(query):
     add_results(fts_results)
     add_results(vector_results)
 
-    final_results = sorted(
+    final_results = sorted(dnaisnds
         rrf_scores.values(),
         key=lambda x: x["score"],
         reverse=True
